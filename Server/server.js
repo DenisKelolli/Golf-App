c2a2dfbf06da43b459b8eae2164d3fb0607422a0
Denis Kelolli <deniskelolli@gmail.com>
2023-08-15 11:55:45 -0400

    Connected multiple users on a page using Socket.io

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -8,31 +10,33 @@ const cors = require("cors");
 const Courses = require("./models/courses");
 const Users = require("./models/users");
 require("dotenv").config();
-const Games = require('./models/games')
+const Games = require('./models/games');
 
 const port = "3000";
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.use(express.json());
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
-// Session handling
 app.use(expressSession({
   secret: process.env.EXPRESS_SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }));
 
-// Passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Local strategy
 passport.use(new LocalStrategy(async (username, password, done) => {
   try {
     const user = await Users.findOne({ username });
@@ -44,7 +48,6 @@ passport.use(new LocalStrategy(async (username, password, done) => {
     return done(err);
   }
 }));
-
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -60,14 +63,56 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-// Login route
+const playersByCourse = {};
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('joinCourse', (player) => {
+    const courseName = "HighLand Greens";
+
+    if (!playersByCourse[courseName]) {
+      playersByCourse[courseName] = [];
+    }
+
+    const existingPlayer = playersByCourse[courseName].find(p => p.name === player.name);
+
+    if (!existingPlayer) {
+      playersByCourse[courseName].push({ ...player, socketId: socket.id });
+    }
+
+    io.emit('newPlayerJoined', playersByCourse[courseName]);
+    console.log(`New player ${player.name} has joined ${courseName}`);
+    console.log('Current players:', playersByCourse[courseName]); // Display current players
+  });
+  
+  socket.on('scoreUpdate', (data) => {
+    const { courseName, playerIndex, holeIndex, score } = data;
+  
+    // Update the corresponding player's score
+    playersByCourse[courseName][playerIndex].scores[holeIndex] = score;
+  
+    // Notify all clients about the score update
+    io.emit('scoreUpdate', playersByCourse[courseName]);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+
+    for (const course in playersByCourse) {
+      playersByCourse[course] = playersByCourse[course].filter(player => player.socketId !== socket.id);
+      io.emit('newPlayerJoined', playersByCourse[course]);
+    }
+  });
+});
+
 app.post('/signin', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) return next(err);
     if (!user) return res.status(401).send('Authentication failed.');
     req.logIn(user, (err) => {
       if (err) return next(err);
-      req.session.userId = user._id; // Store user's ID in session
+      req.session.userId = user._id;
       return res.status(200).send('Authenticated');
     });
   })(req, res, next);
@@ -75,13 +120,8 @@ app.post('/signin', (req, res, next) => {
 
 app.get('/getusersname', async (req, res) => {
   try {
-    // Retrieve the user's ID from the session
     const userId = req.session.userId;
-
-    // Find the user in the Users collection by their ID
     const user = await Users.findById(userId);
-
-    // Respond with the user's name
     res.json({ name: user.name });
   } catch (error) {
     console.error('Error retrieving user name:', error);
@@ -89,12 +129,10 @@ app.get('/getusersname', async (req, res) => {
   }
 });
 
-// Middleware to ensure authentication
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    console.log("User is not Logged in and can't access this page");
     return res.status(403).send("User is not Logged in and can't access this page");
   }
 }
@@ -102,90 +140,81 @@ function ensureAuthenticated(req, res, next) {
 app.get('/courseselection',ensureAuthenticated,  (req, res) => {
   Courses.find()
     .then(courses => {
-      console.log('Courses retrieved:', courses);
-      // Extract only the course names and send them
       const courseNames = courses.map(course => course.courseName);
-      res.send(courseNames); //Send back an array of courseNames
+      res.send(courseNames);
     })
     .catch(err => {
-      console.error('Error retrieving courses:', err); 
       res.status(500).send('Error retrieving courses');
     });
 });
 
-
-app.get('/highlandgreens',ensureAuthenticated,  (req, res) => {
-  // Find the course by name "HighLand Greens" in the Courses collection
-  Courses.findOne({ courseName: "HighLand Greens" })
-    .then(course => {
-      if (!course) {
-        return res.status(404).send('Course not found');
-      }
-
-      // Extract holeNumber and parNumber values from each hole
-      const holesData = course.holes.map(hole => ({
-        holeNumber: hole.holeNumber,
-        parNumber: hole.parNumber
-      }));
-
-      console.log('Holes data retrieved:', holesData); 
-      res.send(holesData);
-    })
-    .catch(err => {
-      console.error('Error retrieving holes data:', err); 
-      res.status(500).send('Error retrieving holes data');
-    });
+app.get('/highlandgreens', ensureAuthenticated, async (req, res) => {
+  try {
+    const courseData = await Courses.findOne({ courseName: "HighLand Greens" });
+    res.json(courseData.holes);
+  } catch (error) {
+    console.error('Error fetching course data:', error);
+    res.status(500).send('Error fetching course data');
+  }
 });
 
+app.get('/highlandgreens/players', ensureAuthenticated, async (req, res) => {
+  try {
+    const games = await Games.findOne({ courseName: "HighLand Greens" });
+    if (games && games.players) {
+      return res.status(200).json(games.players);
+    }
+    return res.status(200).json([]);
+  } catch (err) {
+    console.error('Error retrieving players:', err);
+    return res.status(500).send('Error retrieving players');
+  }
+});
 
-app.post('/highlandgreens', async (req, res) => {
+app.post('/highlandgreens', ensureAuthenticated, async (req, res) => {
   try {
     const players = req.body.players;
     const courseName = req.body.courseName;
     
-    // Create a new game with the provided players' data and course name
-    const game = new Games({
+    const existingGame = await Games.findOne({ courseName });
+    if (existingGame) {
+      existingGame.players.push(...players);
+      await existingGame.save();
+      io.emit('newPlayerJoined'); // Notify all clients about the new player
+      return res.status(201).json({ message: 'Game updated successfully!' });
+    }
+
+    const gameInDb = new Games({
       courseName, 
       players: players.map(player => ({
         playerName: player.playerName, 
         scores: player.scores,
       })),
     });
-
-    await game.save(); // Save the game to the database
-
+    await gameInDb.save();
     res.status(201).json({ message: 'Game saved successfully!' });
   } catch (error) {
-    console.error('Error saving game:', error);
     res.status(500).json({ message: 'Failed to save game. Please try again.' });
   }
 });
 
 app.post('/register',  async (req, res) => {
   const { name, username, password } = req.body;
-
-  // Check if the username already exists
   const existingUser = await Users.findOne({ username });
   if (existingUser) {
     return res.json({ usernameExists: true });
   }
-
-  // Encrypt the password
   const salt = await bcrypt.genSalt(10);
   const encryptedPassword = await bcrypt.hash(password, salt);
-
-  // Save to the Users collection in MongoDB
   const newUser = new Users({
     name,
     username,
     password: encryptedPassword,
   });
-
   try {
     await newUser.save();
     res.status(201).send('User registered successfully');
   } catch (error) {
-    console.error('Error saving user:', error);
     res.status(500).send('Error registering user');
   }
 });
@@ -193,7 +222,6 @@ app.post('/register',  async (req, res) => {
 app.get('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      console.error('Failed to destroy session:', err);
       res.status(500).send('Failed to logout');
     } else {
       res.status(200).send('Logged out successfully');
@@ -209,12 +237,12 @@ const start = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Connected to MongoDB'); 
-    app.listen(port, () => {
+    console.log('Connected to MongoDB');
+    server.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (e) {
-    console.log('Could not connect to MongoDB...', e.message); 
+    console.log('Could not connect to MongoDB...', e.message);
   }
 };
 
